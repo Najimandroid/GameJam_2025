4f16d293228ec227e05b51057c6eb49c5d8d2969
NajimGTECH <nbakkali@gaming.tech>
2025-11-27 18:12:40 +0100

    Initial commit

---
+++ Engine/src/main.cpp
@@ -0,0 +1,51 @@
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+
+int main() 
+{
+    sf::RenderWindow window(sf::VideoMode({ 500, 500 }), "IDF Map");
+    ImGui::SFML::Init(window);
+
+    sf::Clock deltaClock;
+    float deltaTime = 0.f;
+
+    sf::CircleShape circle(100);
+    circle.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) 
+    {
+        while (const auto event = window.pollEvent()) 
+        {
+            ImGui::SFML::ProcessEvent(window, *event);
+
+            if (event->is<sf::Event::Closed>()) 
+            {
+                window.close();
+            }
+        }
+
+		deltaTime = deltaClock.getElapsedTime().asSeconds();
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Test");
+
+        ImGui::Text("ImGui Test");
+
+        ImGui::End();
+
+        window.clear();
+
+        window.draw(circle);
+
+        ImGui::SFML::Render(window);
+
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+}

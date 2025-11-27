2b888214d8eb0e1107162964b44c505359214478
yhoshiino <npeygourdi@gaming.tech>
2025-11-27 21:48:55 +0100

    Player done

--- Engine/Player.cpp
+++ Engine/Player.cpp
@@ -1,5 +1,8 @@
 #include "Player.h"
 
+Player::Player(sf::Texture& _texture, sf::Vector2f _pos_, float _speed) : speed(_speed), Entity(_texture, _pos_) {
+
+}
 
 
 void Player::handleInput()
@@ -15,4 +18,31 @@ void Player::handleInput()
 		velocity.y -= 1.f;
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
 		velocity.y += 1.f;
+}
+
+sf::FloatRect Player::getHB()
+{
+	return sf::FloatRect();
+}
+
+float Player::getSpeed()
+{
+	return speed;
+}
+
+void Player::setSpeed(float newSpeed)
+{
+	speed = newSpeed;
+}
+
+
+
+void Player::update(float dt)
+{
+	handleInput();
+}
+
+void Player::draw(sf::RenderWindow& window)
+{
+	/*window.draw(sprite);*/
 }
--- Engine/Player.h
+++ Engine/Player.h
@@ -9,11 +9,19 @@ private:
 	sf::Vector2f velocity = { 0,0 };
 
 public:
+
+	Player(sf::Texture& texture, sf::Vector2f pos_, float _speed);
+
 	void update(float dt);
+
 	void draw(sf::RenderWindow& window);
+
 	void handleInput();
+
 	sf::FloatRect getHB();
+
 	float getSpeed();
+
 	void setSpeed(float newSpeed);
 };
 
